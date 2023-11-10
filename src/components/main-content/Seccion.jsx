9467f57884617241508b47f9195e407c4867e268
Jhonson Medina <jhonsonamedinah@gmail.com>
2023-11-10 10:29:20 -0300

    mejorando imagen

--- src/components/main-content/Seccion.jsx
+++ src/components/main-content/Seccion.jsx
@@ -1,6 +1,6 @@
 import imagen from '../assets/OIP3.jpg';
 import imagen1 from '../assets/OIP2.jpg';
-import imagen4 from '../assets/images.jpg';
+import imagen4 from '../assets/img/solar/rrrr.webp'
 import imagen5 from '../assets/img/solar/Historia-paneles-solares.jpg';
 import imagen3 from '../assets/img3.webp';
 import fondo from '../assets/IMG-20231102-WA0015.jpg';
